a65afac0659c41a8307c992572344d19f2bf9948
Yuda Setyo Hidayat <yudasetyo07@gmail.com>
2024-06-07 13:33:42 +0700

    feat: Complete Update Product Controller

--- controller/update_product.php
+++ controller/update_product.php
@@ -1,3 +1,23 @@
 <?php 
-    
+    require_once '../utilities/config.php';
+    require_once '../utilities/Database.php';
+    require_once '../utilities/Product.php';
+
+    if ($_POST) {
+        $database = new Database();
+        $db = $database->getConnection();
+        $product = new Product($db);
+
+        $product->id = $_POST['id'];
+        $product->name = $_POST['name'];
+        $product->stock = $_POST['stock']; 
+        $product->category = $_POST['category']; 
+
+        if ($product->update()) {
+            header('location: ../view/index.php');
+        } else {
+            echo "Process Failed";
+        }
+        
+    }
 ?>

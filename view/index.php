80738ff88d892503dcb44909705a0a6b31a3a6e1
Yuda Setyo Hidayat <yudasetyo07@gmail.com>
2024-06-07 13:51:36 +0700

    feat: Setup For Auth (View, Class, Controller)

--- view/index.php
+++ view/index.php
@@ -20,6 +20,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
+            </a>
+        </div>
+    </nav>
     <div class="container">
         <h1>Data Barang</h1>
         <a href="create.php" class="btn btn-primary">Tambah Data</a>
